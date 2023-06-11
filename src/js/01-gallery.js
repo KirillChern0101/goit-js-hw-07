865d9a7733daf5d26e6af3aeeb9cdf6c6295a7fe
KirillChern <kirillchernliko@gmail.com>
2023-06-11 13:29:29 +0300

    hw1+

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,54 +1,7 @@
-// import { galleryItems } from "./gallery-items.js";
-
-// console.log(galleryItems);
-// // Change code below this line
-
-// const gallery = document.querySelector(".gallery");
-// const items = [];
-
-// galleryItems.forEach((element) => {
-//   const galleryItem = document.createElement("div");
-//   galleryItem.className = "gallery__item";
-//   const galleryLink = document.createElement("a");
-//   galleryLink.className = "gallery__link";
-//   galleryLink.href = element.original;
-//   const galleryImage = document.createElement("img");
-//   galleryImage.className = "gallery__image";
-//   galleryImage.src = element.preview;
-//   galleryImage.setAttribute("data-source", element.original);
-//   galleryImage.alt = element.description;
-
-//   galleryItem.append(galleryLink);
-//   galleryLink.append(galleryImage);
-//   items.push(galleryItem);
-// });
-
-// gallery.append(...items);
-
-// gallery.addEventListener("click", (e) => {
-//   e.preventDefault();
-//   if (e.target.nodeName !== "IMG") {
-//     return;
-//   }
-
-//   const selectedImage = e.target.getAttribute("data-source");
-
-//   const instance = basicLightbox.create(`
-//     <img src="${selectedImage}" width="800" height="600">
-// `);
-
-//   instance.show();
-
-//   gallery.addEventListener("keydown", (e) => {
-//     if (e.key === "Escape") {
-//       instance.close();
-//     }
-//   });
-// });
-
 import { galleryItems } from "./gallery-items.js";
 
 console.log(galleryItems);
+// // Change code below this line
 const gallery = document.querySelector(".gallery");
 const markup = createGalleryItemsMarkup(galleryItems);
 gallery.insertAdjacentHTML("beforeend", markup);
@@ -57,29 +10,29 @@ gallery.addEventListener("click", onClick);
 function createGalleryItemsMarkup(items) {
   return items
     .map(({ preview, original, description }) => {
-      return `<li class="gallery_item">
-    <a class="gallery_link" href="${original}">
+      return `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
       <img
-      class="gallery_image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-      ></img>
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
     </a>
-    </li>`;
+  </li>`;
     })
     .join("");
 }
 
 function onClick(evt) {
   evt.preventDefault();
-  if (evt.target.nodName !== "IMG") return;
+  if (evt.target.nodeName !== "IMG") return;
 
-  const isItemImage = evt.target.classlist.contains("gallery_image");
+  const isItemImage = evt.target.classList.contains("gallery__image");
   if (!isItemImage) return;
   const imgUrl = evt.target.dataset.source;
   const instance = basicLightbox.create(
-    `<img src="${imgUrl}" width="1200" height="auto" />`,
+    `<img src="${imgUrl}" width="1280" height="auto"/>`,
 
     {
       onShow: (instance) => {
